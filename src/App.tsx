7f9af164a3d6fe666b978c318c0743391b8b9d18
kundix7-sys <kundix7@gmail.com>
2025-12-17 03:11:58 +0100

    Create App component with ProductTesting integration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ProductTesting } from './components/ProductTesting';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductTesting />
+    </div>
+  );
+}
+
+export default App;
